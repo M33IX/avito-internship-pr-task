442c73489c1abfb441ac05f754b7c664ce31d9b8
Andrew <sgtsh4ksw@gmail.com>
2026-07-07 22:56:39 +0300

    ref(domain): move table constraints to domain layer

--- src/infrastructure/postgres/models.py
+++ src/infrastructure/postgres/models.py
@@ -17,13 +17,14 @@ from sqlalchemy import (
 )
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
+from core.domain.constraints import (
+    PULL_REQUEST_ID_LENGTH,
+    PULL_REQUEST_NAME_LENGTH,
+    TEAM_NAME_LENGTH,
+    USER_ID_LENGTH,
+    USERNAME_LENGTH,
+)
 from core.domain.enums.pull_requests import PRStatus
-
-USER_ID_LENGTH = 20
-TEAM_NAME_LENGTH = 100
-USERNAME_LENGTH = 100
-PULL_REQUEST_ID_LENGTH = 20
-PULL_REQUEST_NAME_LENGTH = 100
 
 
 class Base(DeclarativeBase):
